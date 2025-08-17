6880e95cb968e386560b41d34c9b3af3ffe8dc5a
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2025-08-17 22:10:32 +0100

    Remove: Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,6 @@ import { Box, useMediaQuery } from "@mui/material";
 import "./App.css";
 
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import ProductPage from "./pages/ProductPage";
 import { AppThemeProvider } from "./components/Themes/theme";
@@ -21,7 +20,6 @@ const PageLayout = () => (
   <Box>
     <Header />
     <Outlet />
-    <Footer />
   </Box>
 );
 
