3ed7530a2bd0e38600ac363479ca8168824e2f08
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2025-08-17 22:14:41 +0100

    Update: Styles and themes

--- client/src/components/Themes/theme.tsx
+++ client/src/components/Themes/theme.tsx
@@ -18,13 +18,15 @@ const THEME_SETTINGS_LIGHT = createTheme({
     },
     text: {
       primary: '#1b1b1b',
-      secondary: '#666666',
+      secondary: '#43001B',
+      //@ts-expect-error neutral
+      neutral: '#ffffff'
     },
     background: {
-      default: '#FCF6F8',
-      paper: '#FFE7EF',
+      default: '#FFFFFF',
+      paper: '#FFE1EE',
       //@ts-expect-error neutral
-        neutral: "#ffffff"
+      neutral: "#FCF6F8"
     },
     action: {
       active: '#1b1b1b',
@@ -33,49 +35,49 @@ const THEME_SETTINGS_LIGHT = createTheme({
 });
 
 const THEME_SETTINGS_DARK = createTheme({
-    typography: {
-      fontFamily: "'Inter', sans-serif",
-      fontSize: 14,
+  typography: {
+    fontFamily: "'Inter', sans-serif",
+    fontSize: 14,
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      // main: "#0084e3",
+      main: "#f3f3f3",
+      // main: "#f9eef6",
     },
-    palette: {
-      mode: "dark",
-      primary: {
-        // main: "#0084e3",
-        main: "#f3f3f3",
-        // main: "#f9eef6",
-      },
-      secondary: {
-        main: "#e47094",
-      },
-      text: {
-        primary: '#f3f3f3',
-        secondary: '#CECECE',
-      },
-      background: {
-        default: '#1b1b1b',
-        paper: "#FCF6F8",
-        //@ts-expect-error neutral
-        neutral: "#323232"
-      },
-      action: {
-        active: '#f3f3f3',
-      },
+    secondary: {
+      main: "#e47094",
     },
+    text: {
+      primary: '#f3f3f3',
+      secondary: '#CECECE',
+    },
+    background: {
+      default: '#1b1b1b',
+      paper: "#FCF6F8",
+      //@ts-expect-error neutral
+      neutral: "#323232"
+    },
+    action: {
+      active: '#f3f3f3',
+    },
+  },
 });
 
-function AppThemeProvider({children, mode}:{
+function AppThemeProvider({ children, mode }: {
   children: ReactNode
   mode?: string
 }) {
-  
+
   const [panelTheme] = useState(mode || "dark");
 
   return (
-    <ThemeProvider theme={panelTheme === "dark"? THEME_SETTINGS_DARK : THEME_SETTINGS_LIGHT} >
+    <ThemeProvider theme={panelTheme === "dark" ? THEME_SETTINGS_DARK : THEME_SETTINGS_LIGHT} >
       {children}
     </ThemeProvider>
   )
 
 }
 
-export {AppThemeProvider}
+export { AppThemeProvider }
