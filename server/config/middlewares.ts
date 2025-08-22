ab046b27d5284f387a0b60e9c329923f7fa15ebf
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2025-08-22 13:40:48 +0100

    Update: Server

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -9,8 +9,4 @@ export default [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::compression",
-    config: { br: true, gzip: true, threshold: 1024 },
-  },
 ];
