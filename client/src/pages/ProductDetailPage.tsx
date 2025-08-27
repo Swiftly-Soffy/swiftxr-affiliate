0fa84aebedd1642755f65afbaaa32d46e068e3dc
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2025-08-27 18:15:35 +0100

    SP1-T333: ProductDetailPage

--- client/src/pages/ProductDetailPage.tsx
+++ client/src/pages/ProductDetailPage.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getProductSlug } from "../api";
-import { Box, Typography, Stack, Button } from "@mui/material";
-import Iconify from "../components/Iconify/iconify";
-import type { Product } from "./related/type";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import { ProductCard } from "./CataloguePage";
+import { Box, Typography, Stack, Button, Grid } from "@mui/material";
 
+import { getProductSlug, getCategories } from "../api";
+import type { Product, Category } from "./related/type";
+import Iconify from "../components/Iconify/iconify";
 import { useResponsiveViewContext } from "../components/providers";
-
 import { getLikedProducts, toggleLikedProduct } from "../utils/Likes";
 
-const apiUrl = import.meta.env.VITE_API_URL;
 
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function ProductDetailPage() {
     const { isMobile } = useResponsiveViewContext();
@@ -19,6 +19,11 @@ export default function ProductDetailPage() {
     const { slug } = useParams<{ slug: string }>();
     const [product, setProduct] = useState<Product | null>(null);
 
+    const [liked, setLiked] = useState(false);
+
+    const [related, setRelated] = useState<Product[]>([]);
+
+    //Handle Product
     useEffect(() => {
         if (slug) {
             getProductSlug(slug).then((data) => {
@@ -28,8 +33,7 @@ export default function ProductDetailPage() {
         }
     }, [slug]);
 
-    const [liked, setLiked] = useState(false);
-
+    //Sync Likes
     useEffect(() => {
         if (product) {
             setLiked(getLikedProducts().includes(product.documentId));
@@ -50,169 +54,211 @@ export default function ProductDetailPage() {
             data: { Likes: newLikes },
         });
 
-        // keep product state in sync with new likes
         setProduct({ ...product, Likes: newLikes });
     };
 
+    //Get related products
+    useEffect(() => {
+        if (!product) return;
+
+        getCategories()
+            .then((categories: Category[]) => {
+                const category = categories.find((cat: Category) =>
+                    cat.products.some((p: Product) => p.id === product.id)
+                );
+
+                if (category) {
+                    const relatedProducts = category.products.filter((p: Product) => p.id !== product.id);
+                    setRelated(relatedProducts);
+                }
+            })
+            .catch(console.error);
+
+    }, [product]);
 
     if (!product) return <p>Loading...</p>;
 
     return (
-        <Stack component="section" direction="column"
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                bgcolor: 'background.default',
-                textAlign: 'center',
-                pb: 0,
-                py: 8
-            }}>
-            {product.SwiftXREmbed && (
-                <Box flex={1} width="100%" bgcolor={'background.default'} borderRadius={2}
-                    dangerouslySetInnerHTML={{ __html: product.SwiftXREmbed }} />
-            )}
+        <Stack component="section" bgcolor="background.default">
+            <Stack spacing={6}
+                sx={{
+                    width: 1,
+                    height: 1,
+                    p: 3,
+                }}>
 
-            <Stack bgcolor="background.neutral" width="100%" py={5} mt={-8} borderRadius={2} spacing={1} alignItems="center">
-
-                <Stack direction="row" spacing={1}
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <Typography
-                        fontSize={10}
-                        color="text.primary"
-                        fontWeight={700}
-                        sx={{
-                            textDecoration: "none",
-                            bgcolor: 'background.paper',
-                            color: '#FF3D8A',
-                            borderRadius: 10,
-                            px: 4,
-                            py: 0.7,
-                            width: 'fit-content'
-                        }}
-                    >
-                        By {product.StoreName ?? "Unknown Store"}
-                    </Typography>
-
-                    <Box display="flex" position="relative" alignItems="center" justifyContent="center">
-                        <Box
-                            bgcolor="background.neutral"
-                            p={1}
-                            borderRadius="50%"
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                border: 1,
-                                borderColor: "grey.300",
-                                width: 30,
-                                height: 30
-                            }}
-                        >
-                            <Iconify icon="ion:heart"
-                                sx={{
-                                    color: '#E21B1B',
-                                    width: '5px',
-                                    height: '5px',
-                                    left: 5.5,
-                                    top: 6,
-                                    position: 'absolute',
-
-                                }} />
-                        </Box>
-
-                        <Box
-                            bgcolor="background.neutral"
-                            p={1}
-                            borderRadius="50%"
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                border: 1,
-                                borderColor: "grey.300",
-                                width: 30,
-                                height: 30,
-                                position: 'absolute',
-                                left: 25
-                            }}
-                        >
-                            <Iconify icon="lets-icons:eye-duotone"
-                                sx={{
-                                    width: '2px',
-                                    height: '2px',
-                                    right: 22,
-                                    top: 5,
-                                    position: 'absolute',
-                                }} />
-                        </Box>
-                    </Box>
-                    <Typography pl={4} fontSize={14} fontWeight={600} color="grey.500">
-                        ({product.Likes ?? 0} || {product.Views ?? 0})
-                    </Typography>
-                </Stack>
-
-                <Typography fontWeight={500} fontSize={32} color="text.secondary">
-                    {product.Name}
-                </Typography>
-
-                <Typography fontWeight={400} fontSize={14} px={10} width={isMobile? '100%': '70%'} color="text.primary">
-                    {product.Description}
-                </Typography>
-
-                <Box display="flex" flexDirection="row" alignItems="flex-end" justifyContent="center" gap={2}>
-                    {product.ExternalUrl && (
-                        <Button
-                            component="a"
-                            href={product.ExternalUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            sx={{
-                                backgroundColor: "#0084E3",
-                                color: "text.neutral",
-                                textDecoration: "none",
-                                mt: 2,
-                                borderRadius: 2,
-                                px: 15,
-                                py: 1.5,
-                                "&:hover": {
-                                    bgcolor: '#006bbf',
-                                    color: 'text.neutral'
-                                },
-                            }}
-                        >
-                            Buy Now
-                        </Button>
-                    )}
+                {/* Product Embed */}
+                {product.SwiftXREmbed && /<iframe|<embed/.test(product.SwiftXREmbed) ? (
                     <Box
+                        flex={1}
+                        width="100%"
+                        bgcolor="background.neutral"
+                        borderRadius={2}
+                        dangerouslySetInnerHTML={{ __html: product.SwiftXREmbed }}
+                    />
+                ) : product.Image ? (
+                    <Box
+                        flex={1}
+                        width="100%"
+                        minHeight={300}
+                        bgcolor="background.neutral"
+                        borderRadius={2}
                         display="flex"
                         justifyContent="center"
                         alignItems="center"
-                        sx={{
-                            bgcolor: "background.default",
-                            border: "1px solid",
-                            borderColor: "divider",
-                            borderRadius: 2,
-                            p: 1,
-                            cursor: "pointer",
-                        }}
+                        overflow="hidden"
+                        padding={2}
                     >
-                        <Iconify
-                            icon={liked ? "streamline-flex:heart-solid" : "gridicons:heart-outline"}
-                            onClick={handleLikeClick}
-                            width={28}
-                            sx={{
-                                cursor: "pointer",
-                                color: liked ? "#E21B1B" : "text.neutral",
-                            }}
+                        <img
+                            src={`${apiUrl}${product.Image.url}`}
+                            alt={product.Image.name}
+                            style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
                         />
                     </Box>
+                ) : null}
 
-                </Box>
+                {/* Product Details */}
+                <Stack bgcolor="background.neutral" width="100%" borderRadius={2} alignItems="center" py={3} textAlign="center">
+                    <Stack direction="row" spacing={1}
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <Typography
+                            fontSize={10}
+                            color="text.primary"
+                            fontWeight={700}
+                            sx={{
+                                textDecoration: "none",
+                                bgcolor: 'background.paper',
+                                color: '#FF3D8A',
+                                borderRadius: 10,
+                                px: 4,
+                                py: 0.7,
+                                width: 'fit-content'
+                            }}
+                        >
+                            By {product.StoreName}
+                        </Typography>
+
+                        <Box display="flex" position="relative" alignItems="center" justifyContent="center">
+                            <Box
+                                bgcolor="background.neutral"
+                                p={1}
+                                borderRadius="50%"
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    border: 1,
+                                    borderColor: "grey.300",
+                                    width: 30,
+                                    height: 30
+                                }}
+                            >
+                                <Iconify icon="ion:heart"
+                                    sx={{
+                                        color: '#E21B1B',
+                                        width: '5px',
+                                        height: '5px',
+                                        left: 5.5,
+                                        top: 6,
+                                        position: 'absolute',
+
+                                    }} />
+                            </Box>
+
+                            <Box
+                                bgcolor="background.neutral"
+                                p={1}
+                                borderRadius="50%"
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    border: 1,
+                                    borderColor: "grey.300",
+                                    width: 30,
+                                    height: 30,
+                                    position: 'absolute',
+                                    left: 25
+                                }}
+                            >
+                                <Iconify icon="lets-icons:eye-duotone"
+                                    sx={{
+                                        width: '2px',
+                                        height: '2px',
+                                        right: 22,
+                                        top: 5,
+                                        position: 'absolute',
+                                    }} />
+                            </Box>
+                        </Box>
+
+                        <Typography pl={4} fontSize={14} fontWeight={600} color="grey.500">
+                            ({product.Likes ?? 0} || {product.Views ?? 0})
+                        </Typography>
+                    </Stack>
+
+                    <Typography fontWeight={500} fontSize={32} color="text.secondary">
+                        {product.Name}
+                    </Typography>
+
+                    <Typography fontWeight={400} fontSize={14} px={10} width={isMobile ? '100%' : '70%'} color="text.primary">
+                        {product.Description}
+                    </Typography>
+
+                    <Box display="flex" flexDirection="row" alignItems="flex-end" justifyContent="center" gap={2}>
+                        {product.ExternalUrl && (
+                            <Button
+                                component="a"
+                                href={product.ExternalUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                sx={{
+                                    backgroundColor: "#0084E3",
+                                    color: "text.neutral",
+                                    textDecoration: "none",
+                                    mt: 2,
+                                    borderRadius: 2,
+                                    px: 15,
+                                    py: 1.5,
+                                    "&:hover": {
+                                        bgcolor: '#006bbf',
+                                        color: 'text.neutral'
+                                    },
+                                }}
+                            >
+                                Buy Now
+                            </Button>
+                        )}
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            sx={{
+                                bgcolor: "background.default",
+                                border: "1px solid",
+                                borderColor: "divider",
+                                borderRadius: 2,
+                                p: 1,
+                                cursor: "pointer",
+                            }}
+                        >
+                            <Iconify
+                                icon={liked ? "streamline-flex:heart-solid" : "gridicons:heart-outline"}
+                                onClick={handleLikeClick}
+                                width={28}
+                                sx={{
+                                    cursor: "pointer",
+                                    color: liked ? "#E21B1B" : "text.neutral",
+                                }}
+                            />
+                        </Box>
+
+                    </Box>
+                </Stack>
             </Stack>
 
             {/* Related Product */}
@@ -249,6 +295,22 @@ export default function ProductDetailPage() {
                     />
                 </Box>
             </Box>
+
+            <Grid container spacing={4} sx={{ width: '100%', boxSizing: 'border-box', overflow: 'hidden' }}>
+                {related.length > 0 ? (
+                    related.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))
+                ) : (
+                    <Typography
+                        fontSize={20}
+                        color="text.secondary"
+                        sx={{ textAlign: "center", width: "100%", mt: 5 }}
+                    >
+                        No related Product
+                    </Typography>
+                )}
+            </Grid>
         </Stack>
     );
 }
